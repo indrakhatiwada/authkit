f72d0ff3f3beeeafdbce98bf5be89ffe0b13c6fb
Indra Khatiwada <khatiwadai024@gmail.com>
2024-11-23 01:18:23 +0545

    FIX: types in middleware

--- middleware.ts
+++ middleware.ts
@@ -6,32 +6,31 @@ import {
   publicRoutes,
 } from "./routes";
 
+import { NextResponse } from "next/server";
+
 export default auth((req) => {
   const { nextUrl } = req;
-
   const isLoggedIn = !!req.auth;
-
   const isAPIAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
 
   if (isAPIAuthRoute) {
-    return null;
+    return NextResponse.next();
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+      return NextResponse.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
     }
-
-    return null;
+    return NextResponse.next();
   }
 
   if (!isLoggedIn && !isPublicRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl));
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
   }
 
-  return null;
+  return NextResponse.next();
 });
 
 export const config = {
